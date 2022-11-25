0ffed89e70aaefa7939e26d99fb9df81f0924b8d
ensignrolaren <kelseybarmettler@gmail.com>
2022-11-25 16:39:05 -0700

    version bump

--- blocks.php
+++ blocks.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:  Radical Blocks
  * Description:  A collection of custom blocks
- * Version:      1.0.0
+ * Version:      1.0.1
  * Author:       Kelsey Barmettler
  * Author URI:   https://kelseybarmettler.com/
  * Text Domain:  radical-pack
