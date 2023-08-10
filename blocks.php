b626d61c9b62245f094c30602162f968140d1ec1
ensignrolaren <kelseybarmettler@gmail.com>
2023-08-09 22:12:33 -0700

    version bump

--- blocks.php
+++ blocks.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:  Custom ACF Blocks
  * Description:  A collection of custom blocks
- * Version:      1.1.24
+ * Version:      1.1.25
  * Author:       Kelsey Barmettler
  * Author URI:   https://kelseybarmettler.com/
  * Text Domain:  radical-pack
